40fdd9683a35d80d5c4c670d74d6f30b8b873f51
Jackson <jacksonxie612@gmail.com>
2023-09-27 20:41:29 -0400

    Front-end First stage complete

--- web/src/router/index.js
+++ web/src/router/index.js
@@ -1,6 +1,44 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import PkIndexView from '../views/pk/PkIndexView'
+import RecordIndexView from '../views/record/RecordIndexView'
+import RanklistIndexView from '../views/ranklist/RanklistIndexView'
+import NotFound from '../views/error/NotFound'
+import UserBotIndexView from '../views/user/bot/UserBotIndexView'
 
 const routes = [
+  {
+    path: "/",
+    name: "home",
+  },
+  {
+    path: "/pk/",
+    name: "pk_index",
+    component: PkIndexView,
+  },
+  {
+    path: "/ranklist/",
+    name: "ranklist_index",
+    component: RanklistIndexView,
+  },
+  {
+    path: "/user/bot/",
+    name: "user_bot_index",
+    component: UserBotIndexView,
+  },
+  {
+    path: "/record/",
+    name: "record_index",
+    component: RecordIndexView,
+  },
+  {
+    path: "/404/",
+    name: "404",
+    component: NotFound,
+  },
+  {
+    path: "/:catchAll()",
+    redirect: "/404/"
+  }
 ]
 
 const router = createRouter({
---
+++ web/src/views/pk/PkIndexView.vue
@@ -0,0 +1,20 @@
+<template>
+    <ContentField>
+        PK
+    </ContentField>
+</template>
+
+<script>
+import ContentField from '../../components/ContentField.vue'
+
+export default {
+    components: {
+        ContentField
+    }
+}
+
+</script>
+
+<style scoped>
+
+</style>
