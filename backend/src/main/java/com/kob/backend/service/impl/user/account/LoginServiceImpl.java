41032b0865dd31bc0297ad08ae021b8795a34eba
Jackson <jacksonxie612@gmail.com>
2023-10-04 00:00:05 -0400

    JWT interface settled and ready to implement

---
+++ backend/src/main/java/com/kob/backend/service/impl/user/account/LoginServiceImpl.java
@@ -0,0 +1,14 @@
+package com.kob.backend.service.impl.user.account;
+
+import com.kob.backend.service.user.account.LoginService;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+
+@Service
+public class LoginServiceImpl implements LoginService {
+    @Override
+    public Map<String, String> login(String username, String password) {
+        return null;
+    }
+}
